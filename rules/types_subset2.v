9b1c06f6c82c3d49e927bcfd315c9ed25fde5437
Glyn Webster <glynawe@gmail.com>
2024-05-02 10:36:27 +1200

    types_subset2.v : example of induction principle for nested recursive type.

--- rules/types_subset2.v
+++ rules/types_subset2.v
@@ -1,95 +1,79 @@
-(* A larger subset of the Vanilla type rules in Gallina. *)
 
-(* (Recursively defined Tuple type.) *)
-(* (Vanilla uses records, but they are tuples here just for simplicity. *)
+(* Type system with a recursively defined Tuple type. It is "nested
+  recursion" because there is an intervening type: type_t recurs 
+  inside a list. *)
+
+(* This shows how to create recursive functions over types with nested 
+  recursion and an induction principle to handle proofs for the type. *)
  
 Require Import Coq.Lists.List.
 Import ListNotations.
 Open Scope bool_scope.
 
-
-Inductive vanilla_t : Type :=
+Inductive type_t : Type :=
   | Integer
-  | Boolean
-  | Ref (rt: vanilla_t)
-  | Nil
-  | Array (length: nat) (element: vanilla_t)
-  | OpenArray (element: vanilla_t)
-  | Tuple (elements: list vanilla_t).
+  | Ref (rt: type_t)
+  | Array (length: nat) (element: type_t)
+  | Tuple (elements: list type_t).
 
-
-Fixpoint equal (t1: vanilla_t) (t2: vanilla_t) : bool :=
+Fixpoint Equal (t1: type_t) (t2: type_t) : Prop :=
   match t1, t2 with
-  | Integer, Integer => true
-  | Boolean, Boolean => true
-  | Nil, Nil => true
-  | Ref rt1, Ref rt2 => equal rt1 rt2  
-  | Array len1 et1, Array len2 et2 => Nat.eqb len1 len2 && equal et1 et2
-  | OpenArray et1, OpenArray et2 => equal et1 et2
+  | Integer, Integer => True
+  | Ref rt1, Ref rt2 => Equal rt1 rt2  
+  | Array len1 et1, Array len2 et2 => len1 = len2 /\ Equal et1 et2
+  (* | Tuple es1, Tuple es2 => Forall2 Equal es1 es2 *)
   | Tuple es1, Tuple es2 =>
-    (fix equal_elements xs ys :=   (* this has to be nested ! *)
+    (fix equal_elements xs ys :=   (* this must be done instead *)
       match xs, ys with
-      | t1 :: xs', t2 :: ys' => equal t1 t2 && equal_elements xs' ys'
-      | nil, nil => true
-      | _, _ => false
+      | t1 :: xs', t2 :: ys' => Equal t1 t2 /\ equal_elements xs' ys'
+      | nil, nil => True
+      | _, _ => False
       end
     ) es1 es2
-  | _, _ => false 
+  | _, _ => False 
   end.
 
+Section type_ind_strong.
+  Variable P : type_t -> Prop.
+  Hypothesis Integer_case : P Integer.
+  Hypothesis Ref_case : forall (t: type_t), P t -> P (Ref t).
+  Hypothesis Array_case : forall (l : nat) (t: type_t), Prop -> P t -> P (Array l t).
+  Hypothesis Tuple_case : forall (ts: list type_t), Forall P ts -> P (Tuple ts).
 
-Fixpoint valid_variable (t: vanilla_t) : bool :=
-  match t with
-  | Integer => true
-  | Boolean => true
-  | Ref rt => valid_reference rt
-  | Nil => false
-  | Array len et => Nat.ltb 0 len && valid_variable et
-  | OpenArray _ => false
-  | Tuple es => forallb valid_variable es
-  end
+  Fixpoint type_ind_strong (t : type_t) : P t :=
+    match t with
+    | Integer => Integer_case
+    | Ref t => Ref_case t (type_ind_strong t)
+    | Array l t => Array_case l t (l > 0) (type_ind_strong t)
+    | Tuple ts =>
+      Tuple_case ts 
+        ( ( fix tuple_list_ind (ts : list type_t) : Forall P ts :=
+            match ts with
+            | [] => Forall_nil _
+            | t :: ts' => Forall_cons t (type_ind_strong t) (tuple_list_ind ts')
+            end ) 
+          ts )
+    end.
+End type_ind_strong.
 
-with valid_reference (t: vanilla_t) : bool :=
-  match t with
-  | Integer => true
-  | Boolean => true
-  | Ref rt => valid_reference rt
-  | Nil => false
-  | Array len et => Nat.ltb 0 len && valid_variable et
-  | OpenArray a => valid_variable a
-  | Tuple es => forallb valid_variable es
-  end.
+Theorem Equal_refl : forall (t: type_t), Equal t t.
+Proof.
+  induction t using type_ind_strong; simpl; auto.
+  induction H; auto.
+Qed.
 
-Definition valid_value (t: vanilla_t) : bool :=
-  match t with
-  | Integer => true
-  | Boolean => true
-  | Ref rt => valid_reference rt
-  | Nil => true
-  | Array _ _ => false
-  | OpenArray _ => false
-  | Tuple _ => false
-  end.
-
-
-Definition assignment_compatible (dst: vanilla_t) (src: vanilla_t) : bool :=
-  match dst, src with
-  | Ref _,       Nil         => true
-  | Nil,         _           => false
-  | OpenArray _, _           => false
-  | _,           OpenArray _ => false
-  | t1,          t2          => equal t1 t2
-  end.
-
-Definition var_parameter_compatible (dst: vanilla_t) (src: vanilla_t) : bool :=
-  match dst, src with
-  | OpenArray et1,  Array _ et2  => equal et1 et2
-  | t1,             t2           => equal t1 t2
-  end.
-
-Definition value_parameter_compatible (dst: vanilla_t) (src: vanilla_t) : bool :=
-  match dst, src with
-  | OpenArray et1,  Array _ et2    => equal et1 et2
-  | OpenArray et1,  OpenArray et2  => equal et1 et2
-  | t1,             t2             => assignment_compatible t1 t2
-  end.
+(*
+Theorem Equal_refl : forall (t: type_t), Equal t t.
+Proof.
+  intros.
+  induction t using type_ind_strong; simpl.
+  - reflexivity.
+  - apply IHt.
+  - split. reflexivity. apply IHt.
+  - induction H.
+    + reflexivity.
+    + split.
+      * apply H.
+      * apply IHForall.
+Qed.
+*)  
