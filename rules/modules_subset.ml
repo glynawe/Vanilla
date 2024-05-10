c3cfff767fba67b0f94188669794d4c65d0231ed
Glyn Webster <glynawe@gmail.com>
2024-05-10 14:34:56 +1200

    module_subset.ml

--- rules/modules_subset.ml
+++ rules/modules_subset.ml
@@ -1,66 +1,97 @@
-(* A subset of Vanilla's module system and top-level definitions. *)
-
-module type PROGRAM = 
-sig
-  type t
-  type name_t
-  type module_t
-  val get : t -> name_t -> module_t option
-end
+(* A subset of Vanilla's module system and top-level defs. *)
 
 
-module type MODULE = 
-sig
-  type t
-  type type_t
-  type name_t
-  type globalname_t
-  type definition_t
-
-  val get : t -> globalname_t -> definition_t option
-
-  (* Get the type of a definition. *) 
-  (* None if the name is not defined or not a type. *) 
-  val get_type : t -> globalname_t -> type_t option
-
-  (* True if two type definitions are equivalent. *)
-  (* Either if the names are the same, or are names defined as
-     equivalent by a functor declaration. *)
-  val equivalent : t -> globalname_t -> globalname_t -> bool
-end
-
-
-module type DEFINITION = 
-sig
-  type name_t
-  type type_t
-  type value_t
-
-  type t =
-  | Type of type_t
-  | Var of type_t
-  | Val of type_t * value_t option
-  | Const of type_t * value_t
-  | Procedure of name_t list * type_t
-end
-
-
-module type NAME = 
-sig
+module type EQUALITY = sig
   type t
   val equal : t -> t -> bool
-  val compare : t -> t -> int 
 end
 
 
-module type GLOBALNAME = 
-sig
-  type name_t
-
+module Type (Name: EQUALITY) (GlobalName: EQUALITY) =
+struct
   type t =
-  | Local of name_t
-  | Imported of name_t * name_t
+  | Integer
+  | Nil
+  | Ref of t
+  | Array of int * t
+  | Abstract of GlobalName.t
+  | Record of (Name.t * t) list
 
-  val equal : t -> t -> bool
-  val compare : t -> t -> int 
+  let rec equal (a: t) (b: t) : bool = 
+    match a, b with
+    | Integer, Integer -> true
+    | Nil, Nil -> true
+    | Ref ta, Ref tb -> equal ta tb
+    | Array (la, ta), Array (lb, tb) -> la = lb && equal ta tb
+    | Abstract na, Abstract nb -> GlobalName.equal na nb
+    | Record ra, Record rb -> List.for_all2 equal_elements ra rb 
+    | _, _ -> false
+  and equal_elements (na, ta) (nb, tb) =
+    Name.equal na nb && equal ta tb
+end
+
+
+module Value =
+struct 
+  type t =
+  | Integer of int
+  | Nil
+  (* etc *)
+
+  let value_equal (a: t) (b: t) : bool =
+    match a, b with
+    | Integer a', Integer b' -> a' = b'
+    | Nil, Nil -> true
+    | _, _ -> false
+end
+
+
+module Definition (Name: EQUALITY) (Value: EQUALITY) (Type: EQUALITY) =
+struct 
+  type t =   
+  | Type of Type.t
+  | Var of Type.t
+  | Val of Type.t
+  | Const of Type.t * Value.t
+  | Procedure of Name.t list * Type.t
+
+  let equal (a: t) (b: t) : bool =
+  match a, b with
+  | Type ta, Type tb -> Type.equal ta tb
+  | Var ta, Var tb -> Type.equal ta tb
+  | Val ta, Val tb -> Type.equal ta tb
+  | Const (ta, va), Const (tb, vb) -> 
+      Type.equal ta tb && Value.equal va vb
+  | Procedure (pa, ta), Procedure (pb, tb) -> 
+      Type.equal ta tb && List.length pa = List.length pb 
+  | _, _ -> false
+end
+
+
+module Module (GlobalName: Map.OrderedType) (Definition: EQUALITY) =
+struct
+  module Contents = Map.Make(GlobalName)
+
+  type t = Definition.t Contents.t
+
+  let equal (a: t) (b: t) : bool =
+    Contents.equal Definition.equal a b
+
+  let add_definition (m: t) (n: GlobalName.t) (d: Definition.t) : t option =
+    match Contents.find_opt n m with
+    | None -> Some (Contents.add n d m)
+    | Some dm when Definition.equal dm d -> Some m
+    | Some _ -> None
+
+  let add_interface (m: t) (i: t) : t option =
+    Utility.OptionMonad.fold_option
+      (fun m (n, d) -> add_definition m n d) m (Contents.bindings i)
+        
+  let contains_definition (m: t) (n: GlobalName.t) (d: Definition.t) : bool =
+    match Contents.find_opt n m with
+    | Some dm -> Definition.equal dm d
+    | None -> false
+
+  let contains_interface (m: t) (i: t) : bool =
+    Contents.for_all (contains_definition m) i
 end
